31f7aa46ac417c79c76dce66bc7198620209bd10
pabloxw <pablotachella95@gmail.com>
2021-06-13 18:46:39 -0300

    Se instalaron y configuraron los plugins y loaders de css y sus preprocesadores para agregar estilos al proyecto

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -29,6 +30,14 @@ module.exports = {
 				use: {
 					loader: 'html-loader'
                 }
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -36,6 +45,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
         })
     ],
 }
